3ee394044cfb920d906c46dfc514913c013e5b09
s4gk <s4gk.23@gmail.com>
2025-12-17 09:18:03 -0500

    chore: Initial Dashboard Architecture

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#0ea5e9',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
