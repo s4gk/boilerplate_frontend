35461d9e4543f27c0b776c8eb9a801ba70f120ff
s4gk <s4gk.23@gmail.com>
2026-02-23 10:54:17 -0500

    Fix: correct import path for ForgotPasswordForm and next config typo

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
+  output: "standalone",
 };
 
 export default nextConfig;
